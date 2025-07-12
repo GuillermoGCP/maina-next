71e44006acf88aadf86521018276ba9e242490a2
GuillermoGCP <guillermocporto@gmail.com>
2025-07-12 20:50:39 +0200

    repair trailingSlash error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
 }
 
-export default nextConfig
+module.exports = nextConfig
 
 // pnpm build
-// pnpm export
