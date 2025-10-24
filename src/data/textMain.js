a89f7d50bf8f17dc1ef2f973fe49d927e4d9af6d
GuillermoGCP <guillermocporto@gmail.com>
2025-10-24 16:36:50 +0200

    last updates

--- src/data/textMain.js
+++ src/data/textMain.js
@@ -1,5 +1,5 @@
 const textBlocks = [
-  'Maina es más que un restaurante; es un rincón acogedor donde la cocina de mercado se combina con el cariño de un trato cercano. Nos encontramos en el corazón del barrio de Benimaclet, en Valencia, y ofrecemos una experiencia gastronómica donde cada plato cuenta una historia de tradición, temporada y cercanía.',
+  'Maina es más que un restaurante; es un rincón acogedor donde la cocina de mercado se combina con el cariño de un trato cercano. Ofrecemos una experiencia gastronómica donde cada plato cuenta una historia de tradición, temporada y cercanía.',
   'Nuestra cocina es honesta, saludable y llena de matices. Trabajamos con ingredientes frescos, locales y de temporada, priorizando siempre la calidad y el respeto por el producto. Queremos que cada bocado sea un reflejo de nuestro compromiso con una alimentación consciente y sabrosa.',
 ]
 
